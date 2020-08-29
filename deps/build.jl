e732b085b391eef2ea1753a1def5e41c943f7b06
Filipe Braida <filipebraida@gmail.com>
2020-08-28 22:49:37 -0300

    fixing build

--- deps/build.jl
+++ deps/build.jl
@@ -1,33 +1,16 @@
-using PyCall
+using PyCall: pyimport_conda, pycall
+using RCall, Conda
 
-println("Running build.jl for the SyntheticDatasets package.")
-
-# Change that to whatever packages you need.
-PACKAGES = ["sklearn"]
-
-# Use eventual proxy info
-proxy_arg=String[]
-if haskey(ENV, "http_proxy")
-    push!(proxy_arg, "--proxy")
-    push!(proxy_arg, ENV["http_proxy"])
+function installpypackage()
+	try
+		pyimport_conda("sklearn", "scikit-learn")
+	catch
+		try
+			Conda.add("scikit-learn")
+		catch
+			println("scikit-learn failed to install")
+		end
+	end
 end
 
-# Import pip
-try
-    @pyimport pip
-catch ee
-    typeof(ee) <: PyCall.PyError || rethrow(ee)
-    error("""
-Python Pip not installed
-Please either:
-- Install pip
-- Rebuild SyntheticDatasets.jl via `Pkg.build("SyntheticDatasets")` in the julia REPL
-""")
-end
-
-try
-    @pyimport sklearn
-catch
-    println("Installing required python packages using pip")
-    run(`$(PyCall.python) $(proxy_arg) -m pip install --user $(PACKAGES)`)
-end
+installpypackage()
