217fa214b0bb641c9a4d0dd98a760a43b82c7901
Pedro <pedrooconradoo@gmail.com>
2020-08-31 23:25:57 -0300

    Adding generate regression function and its docstring

--- src/SyntheticDatasets.jl
+++ src/SyntheticDatasets.jl
@@ -74,8 +74,64 @@ function generate_blobs(;n_samples::Union{Int, Array{Int, 1}} = 100,
 
     return convert(features, labels)
 end
+"""
+    generate_regression(;   n_samples=100, 
+                            n_features=100, *, 
+                            n_informative=10, 
+                            n_targets=1, 
+                            bias=0.0, 
+                            effective_rank=None, 
+                            tail_strength=0.5, 
+                            noise::Float=0.0, 
+                            shuffle=True, 
+                            coef=False, 
+                            random_state=None)
+Generate a random regression problem. Sklearn interface to make_regression.
+# Arguments
+- `n_samples = 100`: If int, it is the total number of points equally divided among clusters. If array-like, each element of the sequence indicates the number of samples per cluster. 
+- `n_features = 2`: The number of features for each sample.
+- `n_informative::Int = 10`: The number of informative features, i.e., the number of features used to build the linear model used to generate the output.
+- `n_targets::Int = 1`: The number of regression targets, i.e., the dimension of the y output vector associated with a sample. By default, the output is a scalar.
+- `bias::Float = 0.0`: The bias term in the underlying linear model.
+- `effective_rank::Union{Int, Nothing} = nothing`: If not `nothing`, the approximate number of singular vectors required to explain most of the input data by linear combinations. Using this kind of singular spectrum in the input allows the generator to reproduce the correlations often observed in practice. If `nothing`, the input set is well conditioned, centered and gaussian with unit variance.
+- `tail_strength::Float = 0.5`: The relative importance of the fat noisy tail of the singular values profile if effective_rank is not None.
+- `noise::Union{Nothing, Float64} = nothing`: Standard deviation of Gaussian noise added to the data.
+- `shuffle::Bool = true`: Shuffle the samples.
+- `coef::Bool = false`: If `true`, the coefficients of the underlying linear model are returned.
+- `random_state::Union{Int, Nothing} = nothing`: Determines random number generation for dataset shuffling and noise.
+Reference: [link](https://scikit-learn.org/stable/modules/generated/sklearn.datasets.make_regression.html)
+"""
+function generate_regression(;  n_samples::Int = 100, 
+                                n_features::Int = 100,
+                                n_informative::Int = 10, 
+                                n_targets::Int = 1, 
+                                bias::Float64 = 0.0, 
+                                effective_rank::Union{Int, Nothing} = nothing, 
+                                tail_strength::Float64 = 0.5, 
+                                noise::Float64 = 0.0, 
+                                shuffle::Bool = true, 
+                                coef::Bool = false, 
+                                random_state::Union{Int, Nothing}= nothing)
 
-function convert(features::Array{T, 2}, labels::Array{Int, 1})::DataFrame where T <: Number
+    
+    (features, labels) = datasets.make_regression(   n_samples = n_samples,
+                                                n_features = n_features,
+                                                n_informative = n_informative, 
+                                                n_targets = n_targets,
+                                                bias = bias,
+                                                effective_rank = effective_rank,
+                                                tail_strength = tail_strength,
+                                                noise = noise,
+                                                shuffle = shuffle,
+                                                coef = coef,
+                                                random_state = random_state)
+    
+
+    return convert(features, labels)
+
+end
+
+function convert(features::Array{T, 2}, labels::Array{D, 1})::DataFrame where {T <: Number, D <: Number}
     df = DataFrame()
 
     for i = 1:size(features)[2]
