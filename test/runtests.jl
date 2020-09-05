63e15c52b87e4889edeb98991fc68b25c210ca8d
Nicolas Magalhães <magalhaesnicolas23@gmail.com>
2020-09-05 01:16:11 -0300

    fixed Feature\generate_swiss_roll

--- test/runtests.jl
+++ test/runtests.jl
@@ -37,3 +37,12 @@ using Test
     @test size(data)[2] == features + 1
 
 end
+data = SyntheticDatasets.generate_swiss_roll(n_samples =samples,
+                                             noise = 2.2,
+                                             random_state = 5)
+
+ @test @show size(data)[1] == samples
+ @test size(data)[2] == 4
+
+
+end 
