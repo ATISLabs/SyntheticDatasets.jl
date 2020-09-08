251561b914d9e7ebfeb0cc459511569d03761284
Sergio Felipe Rezende <sergiorezende2001@gmail.com>
2020-09-08 14:50:58 -0300

    Adding Matlab testset
    Forgot to create matlab testset in last commit, so did it now.

--- test/runtests.jl
+++ test/runtests.jl
@@ -44,11 +44,14 @@ using Test
     @test size(data)[1] == samples
     @test size(data)[2] == features + 1
 
+end
+
+@testset "Matlab Generators" begin
+    samples = 20000
 
     data = SyntheticDatasets.generate_twospirals(n_samples = samples,
                                                  noise = 2.2)
 
 
     @test size(data)[1] == samples
-
 end
